e633532bcb7ab9d1735549909ee8884dc655219a
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-08 16:15:39 -0500

    add app.get with message to show that we are connected to express

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => {
+  response.send('no place like home');
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
