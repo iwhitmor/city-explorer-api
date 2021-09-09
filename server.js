1f97afd251b24847dff443facb8f9ff2f4ef14af
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-09 16:22:32 -0500

    install axios

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 
+const axios = require('axios');
+
 const app = express();
 const weatherData = require('./data/weather.json');
 
@@ -16,6 +18,14 @@ const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server started: http://localhost:${PORT}`);
+});
+
+const results = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={WEATHER_API_KEY}`, {
+  params: {
+    key: process.env.WEATHER_API_KEY,
+    lat,
+    lon,
+  }
 });
 
 class Forecast {
